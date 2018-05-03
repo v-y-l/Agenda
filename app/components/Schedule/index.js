cff9fa5553fda83b03ffc4938b4a6424e17abe8c
Victor Lin <vlin@nextjump.com>
2018-05-03 17:37:49 -0400

    Start working on schedule ui

--- app/components/Schedule/index.js
+++ app/components/Schedule/index.js
@@ -7,13 +7,15 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
-
+import ScheduleItem from 'components/ScheduleItem';
+ 
 
 class Schedule extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
       	Schedule
+      	<ScheduleItem />
       </div>
     );
   }
--- app/components/ScheduleItem/index.js
+++ app/components/ScheduleItem/index.js
@@ -6,14 +6,20 @@
 
 import React from 'react';
 // import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
+const Item = styled.div`
+	padding-left: 10px;
+	padding-right: 10px;
+	background: blue;
+`;
 
 class ScheduleItem extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-      </div>
+      <Item>
+      	ScheduleItem
+      </Item>
     );
   }
 }
