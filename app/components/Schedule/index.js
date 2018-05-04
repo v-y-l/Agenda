4763cdbfb57af94c982eab52cfc4660ecc1377e5
Victor Lin <vlin@nextjump.com>
2018-05-04 09:54:47 -0400

    Adjust color scheme

--- app/components/Schedule/index.js
+++ app/components/Schedule/index.js
@@ -14,15 +14,28 @@ const Container = styled.div`
 	width: 70%;
 `;
 
+const backgroundColor = {
+	judge: "#10A296",
+	present: "#FA987D",
+	leavefeedback: "rgb(234,96,99)",
+	//run: "#72CCA7"
+};
+
+const getBackground = (title) => {
+	return backgroundColor[title.replace(/\s+/g,'').toLowerCase()];
+}
+
 class Schedule extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+
+
     return (
       <Container>
-      	<ScheduleItem title={"Judge"} time={3} background={"#72CCA7"} />
-      	<ScheduleItem title={"Judge"} time={3} background={"#72CCA7"} />
-      	<ScheduleItem title={"Judge"} time={3} background={"#10A296"} />
-      	<ScheduleItem title={"Judge"} time={3} background={"#FA987D"} />
-      	<ScheduleItem title={"Judge"} time={3} background={"rgb(234,96,99)"} />
+      	<ScheduleItem title={"Judge"} time={3} background={getBackground("Judge")} />
+      	<ScheduleItem title={"Judge"} time={3} background={getBackground("Judge")} />
+      	<ScheduleItem title={"Judge"} time={3} background={getBackground("Judge")} />
+      	<ScheduleItem title={"Present"} time={3} background={getBackground("Present")} />
+      	<ScheduleItem title={"Leave Feedback"} time={3} background={getBackground("Leave Feedback")} />
       </Container>
     );
   }
