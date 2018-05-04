b1adba8ca8c6cf1bcd67751de18a4970afbf8ef2
Victor Lin <vlin@nextjump.com>
2018-05-04 15:31:19 -0400

    Complete basic card UI

--- app/components/EventCard/index.js
+++ app/components/EventCard/index.js
@@ -9,10 +9,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Card = styled.div`
+	font-family: 'Raleway', sans-serif;
+	letter-spacing: 2px;
+	color: #f0f0f0;
 	margin-top: 45px;
 	display:flex;
 	flex-direction:column;
-	justify-content: space-around;
+	justify-content: space-evenly;
 	align-items: center;
 	background: ${props=>props.backgroundColor};
 	width: 50%;
@@ -21,14 +24,14 @@ const Card = styled.div`
 `;
 
 const Section = styled.div`
-	color: green;
+	font-size: ${props=>props.size}px;
+	color: white;
 `;
 
 const backgroundColor = {
 	judge: "#10A296",
 	present: "#FA987D",
 	leavefeedback: "rgb(234,96,99)",
-	//run: "#72CCA7"
 };
 
 const getBackground = (title) => {
@@ -40,8 +43,8 @@ class EventCard extends React.Component { // eslint-disable-line react/prefer-st
   	const { isComplete, time, title } = this.props;
     return (
       <Card backgroundColor={getBackground(title)} >
-      	<Section> { title } </Section>
-      	<Section> { time } </Section>
+      	<Section size={42}> { title } </Section>
+      	<Section size={64}> { time } </Section>
       </Card>
     );
   }
